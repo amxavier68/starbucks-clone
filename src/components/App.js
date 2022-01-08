f04cfd8362640ee6257af909e76fe3f55d3c16d3
Anthony Xavier <anthony-xavier@live.com>
2022-01-08 18:58:59 +1000

    finalized setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../public/css/styles.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Starbucks - Clone</h1>
+    </div>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,54 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
   mode: 'development',
+  target: 'web',
   devtool: false,
 
   entry: './src/index.js',
-  
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      linkType: "text/css"
+    }),
+    new EslintWebpackPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }
+    ],
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  }
+
 };
